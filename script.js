4c9feecaf2d78a35c237c829a1509707505fbf01
Surapa <surapa152@gmail.com>
2022-08-06 23:43:59 +0700

    done

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
 
   //your code here
+  if (inputAdd.value == "") {
+    alert("Todo can not be empty");
+    return;
+  } else {
+    let check = false;
+    addTodo(inputAdd.value, check);
+    inputAdd.value = "";
+    saveTodo();
+  }
 };
 
 function addTodo(title, completed) {
@@ -33,6 +42,30 @@ function addTodo(title, completed) {
   div.append(doneBtn);
   div.append(deleteBtn);
   todoCtn.prepend(div);
+  doneBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+  div.onmouseover = () => {
+    doneBtn.style.display = "";
+    deleteBtn.style.display = "";
+  };
+  div.onmouseout = () => {
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+  };
+
+  doneBtn.onclick = () => {
+    if (span.style.textDecoration === "line-through") {
+      span.style.textDecoration = "";
+    } else {
+      span.style.textDecoration = "line-through";
+    }
+    saveTodo();
+  };
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+    saveTodo();
+  };
+  saveTodo();
   //append todo to HTML...
   //define buttons event...
 }
@@ -48,13 +81,14 @@ function saveTodo() {
     data.push(todoObj);
   }
   //your code here
+  data.reverse();
   localStorage.setItem("todoList", JSON.stringify(data));
 }
 
 function loadTodo() {
   //your code here
   const data = JSON.parse(localStorage.getItem("todoList"));
-  for (const todoObj of data.reverse()) {
+  for (const todoObj of data) {
     addTodo(todoObj.title, todoObj.completed);
   }
 }
